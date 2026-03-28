3bcab70df12374a448ae4833c76ca26de63063d0
Anderson Agames <20242525@itla.edu.do>
2026-03-28 15:47:57 -0400

    CRUD funcional

---
+++ script.js
@@ -0,0 +1,28 @@
+let usuarios = [];
+
+function agregar() {
+    const nombre = document.getElementById("nombre").value;
+
+    if(nombre === "") return;
+
+    usuarios.push(nombre);
+    mostrar();
+}
+
+function mostrar() {
+    const lista = document.getElementById("lista");
+    lista.innerHTML = "";
+
+    usuarios.forEach((user, index) => {
+        lista.innerHTML += `
+        <li>
+            ${user}
+            <button onclick="eliminar(${index})">Eliminar</button>
+        </li>`;
+    });
+}
+
+function eliminar(index) {
+    usuarios.splice(index,1);
+    mostrar();
+}
